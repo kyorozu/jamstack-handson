38e6a38e273cb6837913c4d97e2d89a6a5a5712f
Kentaro Yorozu <k-yorozu@tribeat.com>
2020-02-28 20:53:31 +0900

    first commit

---
+++ components/Form.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <v-form name="contact" method="POST" data-netlify="true" lazy-validation>
+      <v-text-field
+        v-show="false"
+        name="form-name"
+        value="contact"
+      />
+      <v-text-field
+        :counter="10"
+        label="Name"
+        name="name"
+        required
+      />
+
+      <v-text-field
+        label="E-mail"
+        name="message"
+        required
+      />
+      <div data-netlify-recaptcha="true" />
+      <v-btn block color="secondary" dark type="submit">
+        Send
+      </v-btn>
+    </v-form>
+  </div>
+</template>
